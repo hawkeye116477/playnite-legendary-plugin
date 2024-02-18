hawkeye116477/playnite-legendary-plugin

commit 8bbdb30138fc22fa02409275acb20733e152b0a3

Improve repairing games with SDLs
* Remember chosen SDLs during first install
* Allow to uninstall some SDL if user no longer wants it

src/LegendaryGameInstaller.xaml.cs
@@ -404,6 +404,32 @@ namespace LegendaryLibraryNS
                         {
                             ExtraContentLB.ItemsSource = extraContentInfo;
                             ExtraContentBrd.Visibility = Visibility.Visible;
+                            if (InstallData.downloadProperties.downloadAction == DownloadAction.Repair)
+                            {
+                                string[] installedTags = default;
+                                var installedAppList = LegendaryLauncher.GetInstalledAppList();
+                                if (installedAppList != null)
+                                {
+                                    if (installedAppList.ContainsKey(GameID))
+                                    {
+                                        var installedGameData = installedAppList[GameID];
+                                        if (installedGameData.Install_tags != null && installedGameData.Install_tags.Length > 1)
+                                        {
+                                            installedTags = installedGameData.Install_tags;
+                                        }
+                                    }
+                                }
+                                foreach (KeyValuePair<string, LegendarySDLInfo> extraCheckbox in ExtraContentLB.Items)
+                                {
+                                    if (extraCheckbox.Value.Tags.Count > 0)
+                                    {
+                                        if (installedTags.Contains(extraCheckbox.Value.Tags[0]))
+                                        {
+                                            ExtraContentLB.SelectedItems.Add(extraCheckbox);
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
                 }

src/Models/Installed.cs
@@ -13,5 +13,6 @@
         public string Save_path { get; set; }
         public string Title_for_updater { get; set; } = "";
         public Prerequisite Prereq_info { get; set; }
+        public string[] Install_tags { get; set; } = default;
     }
 }

src/LegendaryDownloadManager.xaml.cs
@@ -253,6 +253,10 @@ namespace LegendaryLibraryNS
                         {
                             installCommand.Add("--install-tag=" + singleSelectedContent);
                         }
+                        if (downloadProperties.downloadAction == DownloadAction.Repair)
+                        {
+                            installCommand.Add("--reset-sdl");
+                        }
                     }
                 }
                 installCommand.Add("--skip-dlcs");
@@ -368,7 +372,7 @@ namespace LegendaryLibraryNS
                                 {
                                     diskSpaceErrorDisplayed = true;
                                 }
-                                if (!errorMessage.Contains("old manifest"))
+                                if (!errorMessage.Contains("old manifest") && !errorMessage.Contains("Failed removing directory"))
                                 {
                                     errorDisplayed = true;
                                 }

src/Models/LegendarySDLInfo.cs
@@ -6,7 +6,7 @@ namespace LegendaryLibraryNS.Models
     {
         public string Description { get; set; }
         public string Name { get; set; }
-        public List<string> Tags { get; set; }
+        public List<string> Tags { get; set; } = new List<string>();
         public bool Is_dlc { get; set; } = false;
     }
 }

commit a3e107682e41f410ac4f831b445d3041df783d92

Fix possible crash
Regression from 8bbdb30138fc22fa02409275acb20733e152b0a3

src/LegendaryGameInstaller.xaml.cs
@@ -423,7 +423,7 @@ namespace LegendaryLibraryNS
                                 {
                                     if (extraCheckbox.Value.Tags.Count > 0)
                                     {
-                                        if (installedTags.Contains(extraCheckbox.Value.Tags[0]))
+                                        if (installedTags != null && installedTags.Length > 0 && installedTags.Contains(extraCheckbox.Value.Tags[0]))
                                         {
                                             ExtraContentLB.SelectedItems.Add(extraCheckbox);
                                         }

src/Models/Installed.cs
@@ -13,6 +13,6 @@
         public string Save_path { get; set; }
         public string Title_for_updater { get; set; } = "";
         public Prerequisite Prereq_info { get; set; }
-        public string[] Install_tags { get; set; } = default;
+        public string[] Install_tags { get; set; }
     }
 }