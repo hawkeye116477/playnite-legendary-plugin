84317e13e3c926901dceed7d4bdedbb5d7f6546f
hawkeye116477 <hawkeye116477@gmail.com>
2024-11-06 13:23:02 +0100

    Improve installed model

--- src/Models/Installed.cs
+++ src/Models/Installed.cs
@@ -4,14 +4,14 @@ namespace LegendaryLibraryNS.Models
 {
     public class Installed
     {
-        public string App_name { get; set; }
-        public bool Can_run_offline { get; set; }
-        public string Executable { get; set; }
-        public string Install_path { get; set; }
-        public long Install_size { get; set; }
+        public string App_name { get; set; } = "";
+        public bool Can_run_offline { get; set; } = true;
+        public string Executable { get; set; } = "";
+        public string Install_path { get; set; } = "";
+        public long Install_size { get; set; } = 0;
         public bool Is_dlc { get; set; } = false;
-        public string Title { get; set; }
-        public string Version { get; set; }
+        public string Title { get; set; } = "";
+        public string Version { get; set; } = "";
         public string Save_path { get; set; }
         public Prerequisite Prereq_info { get; set; }
         public List<string> Install_tags { get; set; } = new List<string>();
