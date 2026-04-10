56c6c9ee5e8364815c157a58b828503921a46432
hawkeye116477 <hawkeye116477@gmail.com>
2026-04-10 19:16:51 +0200

    Bump version

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("LegendaryLibrary")]
-[assembly: AssemblyCopyright("Copyright © 2023-2025 hawkeye116477\n Copyright © 2020 Josef Nemec")]
+[assembly: AssemblyCopyright("Copyright © 2023-2026 hawkeye116477\n Copyright © 2020 Josef Nemec")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.13.5.*")]
+[assembly: AssemblyVersion("1.14.0.*")]
