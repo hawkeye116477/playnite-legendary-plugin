a1b408aee1fc52252781766b8c6dcdd431816a75
hawkeye116477 <hawkeye116477@gmail.com>
2026-08-09 21:40:36 +0200

    Update SDK to latest version

--- third_party/PlayniteExtensions/PlayniteRepo/source/playnite/common/extensions/StringExtensions.cs
+++ third_party/PlayniteExtensions/PlayniteRepo/source/playnite/common/extensions/StringExtensions.cs
@@ -56,16 +56,6 @@ namespace System
             return Regex.Replace(str, @"[™©®]", remplacement);
         }
 
-        public static bool IsNullOrEmpty(this string source)
-        {
-            return string.IsNullOrEmpty(source);
-        }
-
-        public static bool IsNullOrWhiteSpace(this string source)
-        {
-            return string.IsNullOrWhiteSpace(source);
-        }
-
         public static string Format(this string source, params object[] args)
         {
             return string.Format(source, args);
