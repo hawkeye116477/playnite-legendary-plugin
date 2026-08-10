de4cdbbdca82c6790c2377c22b3b480a503b7e2a
hawkeye116477 <hawkeye116477@gmail.com>
2026-08-10 20:31:16 +0200

    Hide alternative auth button when logged in
    0a2ee9242328f918612a91afda833f184d5b6d5e

--- src/LegendaryLibrarySettingsView.xaml.cs
+++ src/LegendaryLibrarySettingsView.xaml.cs
@@ -499,13 +499,15 @@ public partial class LegendaryLibrarySettingsView
             AuthStatusTB.Text = LocalizationManager.Instance.GetString(LOC.CommonSignedInAs,
                 new Dictionary<string, IFluentType> { ["userName"] = (FluentString)clientApi.GetUsername() });
             LoginBtn.Content = LocalizationManager.Instance.GetString(LOC.CommonSignOut);
-            LoginBtn.IsChecked = true;
+            LoginBtn.IsChecked = true; 
+            LoginAlternativeBtn.Visibility = Visibility.Collapsed;
         }
         else
         {
             AuthStatusTB.Text = LocalizationManager.Instance.GetString(LOC.ThirdPartyEpicNotLoggedIn);
             LoginBtn.Content = LocalizationManager.Instance.GetString(LOC.ThirdPartyEpicAuthenticateLabel);
             LoginBtn.IsChecked = false;
+            LoginAlternativeBtn.Visibility = Visibility.Visible;
         }
 
         LoginBtn.IsEnabled = true;
